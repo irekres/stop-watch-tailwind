64ae2b274ae5b996a3111ad4794ec5d664d5e545
Irek Dynia <irek.res@gmail.com>
2024-06-28 18:11:42 +0200

    Center clocks

--- src/components/Buttons/LapButton.jsx
+++ src/components/Buttons/LapButton.jsx
@@ -2,11 +2,10 @@ export const LapButton = ({ onLap }) => (
   <button
     type="button"
     onClick={onLap}
-    className="text-yellow-400 hover:text-white border border-yellow-400
-    hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300
-    font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2
-    dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white
-    dark:hover:bg-yellow-400 dark:focus:ring-yellow-900"
+    className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4
+    focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+    me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500
+    dark:focus:ring-blue-800"
   >
     LAP
   </button>
--- src/components/Buttons/StartStopButton.jsx
+++ src/components/Buttons/StartStopButton.jsx
@@ -1,10 +1,10 @@
 export const StartStopButton = ({ isRunning, onStartStop }) => (
   <button
     type="button"
-    className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4
-    focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
-    me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500
-    dark:focus:ring-blue-800"
+    className="text-lime-700 hover:text-white border border-lime-700 hover:bg-lime-800 focus:ring-4
+    focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+    me-2 mb-2 dark:border-lime-500 dark:text-lime-500 dark:hover:text-white dark:hover:bg-lime-700
+    dark:focus:ring-lime-800"
     onClick={onStartStop}
   >
     {isRunning ? "STOP" : "START"}
